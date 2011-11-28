593e4dba562c000ca912df7e653bad0d70bfe151
Soren Malling <soren.malling@gmail.com>
2011-11-28 01:10:08 +0100

    Import of current state

---
+++ ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "extbase_dam".
+#
+# Auto generated 17-05-2010 17:51
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Extbase dam helpers',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Nils Blattner',
+	'author_email' => 'nb@cabag.ch',
+	'author_company' => 'cab AG',
+	'shy' => '',
+	'dependencies' => 'cms,extbase,fluid',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.0.2',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:4:{s:14:"ext_tables.php";s:4:"4f89";s:28:"Classes/Domain/Model/Dam.php";s:4:"d691";s:23:"Classes/Utility/Dam.php";s:4:"fc3d";s:34:"Configuration/TypoScript/setup.txt";s:4:"05fc";}',
+);
+
+?>
